066570fadbe6e367d7bcd4c27ed7288dd65c992d
Peter Goodspeed-Niklaus <peter.r.goodspeedniklaus@gmail.com>
2021-01-11 12:48:11 +0100

    add poetry bin dir to path

---
+++ profile.d/poetry.sh
@@ -0,0 +1,7 @@
+# shellcheck shell=bash
+
+if [ -e "$HOME"/dotfiles/ensurepath.sh ]; then
+    # shellcheck source=../ensurepath.sh
+    source "$HOME"/dotfiles/ensurepath.sh
+    ensurepath "$HOME"/.poetry/bin
+fi
