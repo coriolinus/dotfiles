5645002b892dbda83bc77e297d728609e929bc82
Peter Goodspeed-Niklaus <peter.r.goodspeedniklaus@gmail.com>
2022-07-07 00:20:10 +0200

    force pyenv init when not in brew/osx

--- profile.d/pyenv.sh
+++ profile.d/pyenv.sh
@@ -6,6 +6,14 @@
 # For how to setup / fix the issue, see
 # https://stackoverflow.com/questions/71591971/how-can-i-fix-the-zsh-command-not-found-python-error-macos-monterey-12-3
 
+if [ -e "$HOME"/dotfiles/ensurepath.sh ] && [ -x "$HOME"/.pyenv/bin/pyenv ]; then
+    # shellcheck source=ensurepath.sh
+    source "$HOME"/dotfiles/ensurepath.sh
+    ensurepath "$HOME"/.pyenv/bin
+
+    export PYENV_ROOT="$HOME/.pyenv"
+fi
+
 if command -v pyenv >/dev/null; then
     eval "$(pyenv init --path)"
 fi
